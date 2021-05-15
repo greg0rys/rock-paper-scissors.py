8418fea235a3d9d26f209719c40cab43ae20fb00
Greg <60129025+greg0rys@users.noreply.github.com>
2021-05-14 23:31:08 -0700

    accumulation of game totals
    added accumulator variables to hold games played, games won, games tied.
    add output for the user to see game stats

--- main.py
+++ main.py
@@ -31,27 +31,59 @@ def userChoice():
 
 def main():
     again = 'y'
+    # declare the accumulators outside of the while loop
+    # this way they don't keep resetting to 0 inside the loop
+    # these hold the game stats
+    userWins = 0
+    computerWins = 0
+    gamesPlayed = 0
+    ties = 0
     while again:
         user = userChoice()
         computer = compChoice()
         if user == computer:
             print('It\'s a tie!')
+            userWins = userWins
+            computerWins = computerWins
+            ties += 1
         elif user == 'rock' and computer == 'paper':
             print('You lose!', computer,'covers',user)
+            computerWins = computerWins + 1
+            userWins = userWins
         elif user == 'rock' and computer == 'scissors':
             print('You win!', user,'smashes',computer)
+            userWins = userWins + 1
+            computerWins = computerWins
         elif user == 'paper' and computer == 'rock':
             print('You win', user,'covers',computer)
+            userWins = userWins + 1
+            computerWins = computerWins
         elif user == 'paper' and computer == 'scissors':
             print('You lost!', computer,'slices',user)
+            computerWins = computerWins + 1
+            userWins = userWins
         elif user == 'scissors' and computer == 'rock':
             print('You lost!',computer,'smashes',user)
+            computerWins = computerWins + 1
+            userWins = userWins
         elif user =='scissors' and computer == 'paper':
             print('You won!', user, 'slices', computer)
-        again = input('Go again? y/n')
+            userWins = userWins + 1
+            computerWins = computerWins
+        gamesPlayed += 1
+        again = input('Go again? y/n: ')
         again = again.lower()
-        if again == 'n':
+        # if the user enters anything but y exit the loop
+        if again != 'y':
             break
+    # print the games stats:
+    print('Thanks for playing!')
+    print('----GAME STATS----')
+    print('you played:',gamesPlayed,'games')
+    print('you won:', userWins,'games')
+    print('the computer won:',computerWins,'games')
+    print('you tied:', ties,'times')
+    print('------------------')
 
 main()
 
